29ea5418a062d0787f23fc7187231f2e2c978cc3
Natacha Marlio-Marette <natacha.marlio-marette@etu.univ-tours.fr>
2014-10-16 18:10:05 +0200

    l'utilisateur peut donner les valeurs de sigma s et r à l'exécution

--- Dev/Filtre_Bilateral_Naif/Filtre_Bilateral_Naif/main.cpp
+++ Dev/Filtre_Bilateral_Naif/Filtre_Bilateral_Naif/main.cpp
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 #include "include/CImg.h"
 #include "include/filtrebilateral.h"
@@ -9,13 +10,13 @@
 using namespace cimg_library;
 using namespace std;
 
-// template <typename T>
-// inline std::string to_string (const T& t)
-// {
-//   std::stringstream ss;
-//   ss << t;
-//   return ss.str();
-// }
+template <typename T>
+inline std::string to_string (const T& t)
+{
+  std::stringstream ss;
+  ss << t;
+  return ss.str();
+}
 
 
 int main(int argc, char **argv) {
@@ -24,63 +25,67 @@ int main(int argc, char **argv) {
   clock_t start;
   
   
-  float fsigmaS = 0.1;
-  float fsigmaR = 0.1;
+  float fsigmaS;
+  float fsigmaR;
   
   string nomImg;
    
-//   cout<<"Veuillez saisir la valeur de sigma S (en pourcentage)."<< endl;
-//   cin>> fsigmaS;
-//   cout<<"Veuillez saisir la valeur de sigma R (en pourcentage)."<< endl;
-//   cin>> fsigmaR;
+
   
   cout<<"Veuillez saisir le nom de l'image avec son extension (ex: lena.jpg)" << endl;
   cin>> nomImg; 
   
+  cout<<"Veuillez saisir la valeur de sigma S (en pourcentage)."<< endl;
+  cin>> fsigmaS;
+  cout<<"Veuillez saisir la valeur de sigma R (en pourcentage)."<< endl;
+  cin>> fsigmaR;
+  
+  fsigmaS = fsigmaS/100;
+  fsigmaR = fsigmaR/100;
+  
   CImg<double> img(nomImg.c_str());
  
   string nomFichier = "resultat.txt";
-  ofstream fichier(nomFichier.c_str(), ios::ate);
+  ofstream fichier(nomFichier.c_str(), ios::app);
  
-  FiltreBilateral fb(fsigmaS, fsigmaR, img);
-  
-  
   // FILTRE MOYEN
+  FiltreBilateral moy(fsigmaS, fsigmaR, img);
   cout<< "Filtre moyen : début" << endl;
   start = clock();
-  CImg<double> moyImg = fb.moyennePixel();
+  CImg<double> moyImg = moy.moyennePixel();
   temps = (double)(clock()-start)/(double) CLOCKS_PER_SEC;
   cout<< "Temps d'éxécution : " << temps  << " secondes" << endl;
   cout<< "Filtre moyen : done" << endl;
 
   
   // FILTRE BILATERAL
+  FiltreBilateral fb(fsigmaS, fsigmaR, img);
   cout<< "Filtre bilatéral : début" << endl;
   start = clock();
-  CImg<double> fbImg = fb.applyFilter();
+  CImg<double> fbImg = moy.applyFilter();
   temps = (double)(clock()-start)/(double) CLOCKS_PER_SEC;
   cout<< "Temps d'éxécution : " << temps  << " secondes" << endl;
   cout<< "Filtre bilatéral : done" << endl;
  
-  //   string sigmaS =  to_string(fsigmaS);
-//   string sigmaR = to_string(fsigmaR);
+  string sigmaS =  to_string(fsigmaS);
+  string sigmaR = to_string(fsigmaR);
   
-//   nomImgSave = "filtrebilateral_sigmaS_" + sigmaS + "_sigmaR_" + sigmaR + "_" + nomImg ;   
-//   fbImg.save_bmp("filtrebilateral");
-// 	fbImg.save(nomImgSave.c_str());  
+  string nomImgSave = "filtrebilateral_sigmaS_" + sigmaS + "_sigmaR_" + sigmaR + "_" + nomImg ;   
+  
+  fbImg.save(nomImgSave.c_str());  
   
   
-  //   if(fichier){
-//       fichier << "Nom de l'image : " << nomImg << endl; 
-//       fichier << "Paramètre : " << endl;
-//       fichier << "\t" <<  "Sigma S : " << fsigmaS << endl;
-//       fichier << "\t" << "Sigma R : " << fsigmaR << endl;
-//       fichier << "Tenmps d'éxécution : " << temps << "secondes" << endl;
-//       fichier << "Nom de l'image avec le filtre : " <<  nomImgSave << endl; 
-//       fichier << "----------------------------" <<endl;
-//       
-//       fichier.close();
-//   }
+  if(fichier){
+      fichier << "Nom de l'image : " << nomImg << endl; 
+      fichier << "Paramètre : " << endl;
+      fichier << "\t" <<  "Sigma S : " << fsigmaS << endl;
+      fichier << "\t" << "Sigma R : " << fsigmaR << endl;
+      fichier << "Tenmps d'éxécution : " << temps << "secondes" << endl;
+      fichier << "Nom de l'image avec le filtre : " <<  nomImgSave << endl; 
+      fichier << "----------------------------" <<endl;
+      
+      fichier.close();
+  }
   
   
   
